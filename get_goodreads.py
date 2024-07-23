7608b5a5176fb1cc1cbbf43364c7f007e7e635a5
od107 <jochen.deferme@gmail.com>
2024-07-23 17:17:46 +0200

    cleaned up code

--- get_goodreads.py
+++ get_goodreads.py
@@ -5,44 +5,35 @@ import urllib
 import re
 
 def get_goodreads(folder="c:/temp/ebooks/"):
-    # get titles of files in folder
-    # for each title look up the relevant goodreads page
-    # extract the score and the tags (and read by which friend)
-    # save the results to a csv
 
-    # print(os.listdir(folder))
-    # print(os.path.isfile(folder))
-    # print(os.path.isdir(folder))
+    books = cleanup_filenames(folder)
+    fetch_rating_from_goodreads(books)
+    
 
+def cleanup_filenames(folder):
     books = set()
     for dirpath, dirnames, filenames in walk(folder):
         for filename in filenames:
-
             name_without_filetype = re.sub(r'\.[^.]*$', '', filename)
             name_without_inside_brackets = re.sub("\(.*?\)|\[.*?\]","",name_without_filetype)
-            name_without_digits_by_book = re.sub(r'\d|by|book', '', name_without_inside_brackets, flags=re.IGNORECASE)
-            name_without_special_characters = re.sub(r'_|\.|-|,', ' ', name_without_digits_by_book)
+            name_without_digits_book = re.sub(r'\d|book', '', name_without_inside_brackets, flags=re.IGNORECASE)
+            name_without_special_characters = re.sub(r'_|\.|-|,| by ', ' ', name_without_digits_book)
 
-            # print(name_without_special_characters)
             books.add(name_without_special_characters)
-            # search = urllib.parse.quote_plus(name_without_special_characters)
    
-    print(books)
-    
-    output = "Book, Rating"
+    return books
 
+def fetch_rating_from_goodreads(books):
+    output = "Book, Rating"
     try: 
         for book in books:
             url_prefix = "https://www.goodreads.com/search?utf8=%E2%9C%93&q="
             url_postfix = "&search_type=books"
-            print(book)
             search = urllib.parse.quote_plus(book)
             url = url_prefix + search + url_postfix
-            page = urlopen(url)
+            page = urllib.request.urlopen(url)
             html_bytes = page.read()
             html = html_bytes.decode("utf-8")
-
-            #todo go to subpage for extra info
 
             start_index = html.find("avg rating") - 5
             if start_index == -6:
@@ -53,14 +44,11 @@ def get_goodreads(folder="c:/temp/ebooks/"):
             output = "{}\n {}, {}".format(output, book, rating)
             print("{} - {}".format(book, rating))
             
-            sleep(3)
+            #sleep(3)
 
     finally:    
-
         with open('book_ratings.csv', 'w', encoding='utf8') as file:
             file.write(output)
-            
-
 
 
 def main():
